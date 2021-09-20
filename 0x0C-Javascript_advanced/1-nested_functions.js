018a4d6f90a726bfa19815a446c26e30450628b1
Tarek <kheirtarek@hotmail.fr>
2021-09-20 15:43:40 +0200

    task 1

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,23 @@
+let globalVariable = "Welcome";
+
+function outer() {
+    alert(globalVariable);
+
+    let course = "Holberton";
+    
+    function inner() {
+        alert(globalVariable + " " + course);
+
+        let exclamation = "!";
+
+        function inception() {
+            alert(globalVariable + " " + course + exclamation);
+        }
+
+        inception();
+    }
+
+    inner();
+}
+
+outer();
